b037a18e46f2d57a432a5b0dc8b507947613c17a
lumsilva <lumsilva@uolinc.com>
2020-11-27 16:00:01 -0300

    nova branch

--- app/src/main/java/com/dicedmelon/example/android/MainActivity.java
+++ app/src/main/java/com/dicedmelon/example/android/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
             DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         NumberProvider numberProvider = new NumberProvider();
+
+        viewDataBinding.setAlternative(numberProvider.provideAlternativeNumber());
         viewDataBinding.setNumber(numberProvider.provideNumber());
     }
 }
--- app/src/main/java/com/dicedmelon/example/android/NumberProvider.java
+++ app/src/main/java/com/dicedmelon/example/android/NumberProvider.java
@@ -5,4 +5,8 @@ public class NumberProvider {
     public int provideNumber() {
       return 42;
     }
+
+    public int provideAlternativeNumber() {
+      return 50;
+    }
 }
